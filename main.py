5c6d014f48d19b443b5c6dc81ce3cb2521c3bfda
Mimmo b <mimmo.baluyut@icloud.com>
2021-07-24 14:10:38 -0400

    touched up code using json

--- main.py
+++ main.py
@@ -8,22 +8,13 @@ from os import system
 import random
 import nacl
 import shutil
+import json
 
-boobies = ["https://files.yande.re/sample/5a6459719bf0db921ede0d143acd6783/yande.re%20454287%20sample%20darling_in_the_franxx%20horns%20naked%20nipples%20shadowgrave%20wet%20zero_two_%28darling_in_the_franxx%29.jpg"
-, "https://i.redd.it/dl38eysjsae21.jpg"
-, "https://1.bp.blogspot.com/-yYLpUhOuuXA/W86Ie2HlVXI/AAAAAAAA0M8/5uezeksbEIQ04jHi0m11cctrEGmCQPmIACLcBGAs/w7000/Darling%2Bin%2Bthe%2BFranxx%2B-%2BZero%2BTwo%2BRender%2B9.png"
-, "https://i.redd.it/fapsyap3o2o51.jpg"
-, "https://i.redd.it/b5trvmmyk8n51.jpg"
-]
-butt = ["https://files.yande.re/image/877cbe5d78fad92d91c361b15f38dc60/yande.re%20445214%20anus%20ass%20darling_in_the_franxx%20horns%20naked%20penis%20pussy%20rosaline%20uncensored%20zero_two_(darling_in_the_franxx).jpg"
-, "https://i.redd.it/2pnf07afo1331.jpg"
-, "https://external-preview.redd.it/0xrz-2qzmEbMn98XSLVNArhfHcbS9nWSRmV6b9MaMoA.jpg?auto=webp&s=a2c5d2777c039d0f5c2a907f9c2779d1500578bf"
-, "https://pbs.twimg.com/media/EC_5ULSUUAAadpR?format=jpg&name=medium"]
-pussys = ["https://cdn.donmai.us/original/d8/16/__zero_two_darling_in_the_franxx_drawn_by_ginhaha__d8167f749f0ef3186ce12a0c2d6f087f.png"
-, "https://konachan.com/sample/7ab407f045e64a82006f432af148724e/Konachan.com%20-%20261504%20sample.jpg"
-, "https://img2.gelbooru.com/samples/82/a3/sample_82a35e3396eb21849bb6ee7a6b4018c5.jpg"
-, "https://cdn.donmai.us/sample/d3/11/__zero_two_darling_in_the_franxx_drawn_by_darkmaya__sample-d3113958950d5137ec51880df9a64956.jpg"
-, "https://i.redd.it/fapsyap3o2o51.jpg"]
+configfile = open("config.json")
+configdata = json.load(configfile)
+boobies = configdata["boobies"]
+butt = configdata["butt"]
+pussys = configdata["pussy"]
 
 bot = commands.Bot(command_prefix='02')
 client = discord.Client()
@@ -54,27 +45,31 @@ NSFW :underage:
 async def clear(ctx, amount=5):
     await ctx.channel.purge(limit=amount)
 
-@bot.command(pass_context=True)
-async def pussy(ctx):
-    await ctx.send(random.choice(pussys))
-
 @bot.command()
 async def emoji(ctx):
     await ctx.send(":bread:")
 
+@bot.command(pass_context=True)
+async def pussy(ctx):
+    if ctx.channel.is_nsfw():
+        await ctx.send(random.choice(pussys))
+    else:
+        await ctx.send("There are kids here dont do that sh*t")
+
 @bot.command()
 async def boobs(ctx):
     if ctx.channel.is_nsfw():
-        await ctx.send("There are kids here dont do that sh*t")
-    else:
         await ctx.send(random.choice(boobies))
+    else:
+        await ctx.send("There are kids here dont do that sh*t")
 
 @bot.command()
 async def ass(ctx):
     if ctx.channel.is_nsfw():
-        await ctx.send("There are kids here dont do that sh*t")
-    else:
         await ctx.send(random.choice(butt))
+    else:
+        await ctx.send("There are kids here dont do that sh*t")
+        
 
 @bot.command()
 async def stop(ctx):
@@ -243,4 +238,4 @@ async def everyone(ctx):
     await ctx.send("@everyone")
     await ctx.channel.purge(limit=1)
 
-bot.run("NzU2Mjc0MzAwNzEzMTczMTEy.X2PdVA.oGLFh6ypcZwsKCqiIw99oXfh4-o")
+bot.run(configdata["token"])
